a1e19a61ebde56990e4abee0167ad698551ba4c6
Noah Derckum <Noah.Derckum@gmx.de>
2023-06-12 18:11:51 +0200

    III

--- src/scripts/objects/achievementFrame.js
+++ src/scripts/objects/achievementFrame.js
@@ -16,9 +16,18 @@ export default class AchievementFrame {
 
         let imageTint;
         let textTint;
+        let descriptionTint = 0xffffff;
 
         switch(this.type) {
             case 'kills': imageTint= 0xff1111; textTint= 0xff4444; break;
+            case 'distance': imageTint= 0x11ff11; textTint= 0x44ff44; break;
+            case 'score': imageTint= 0x1111ff; textTint= 0x4444ff; break;
+        }
+
+        if(this.enabled == false) {
+            imageTint= 0x444444; 
+            textTint= 0x888888; 
+            descriptionTint = 0xBBBBBB;
         }
 
         this.image = this.context.add.image(this.x, this.y,'achievementframe');
@@ -33,10 +42,11 @@ export default class AchievementFrame {
 
         this.textTitle = this.context.add.text(this.x+40, this.y+0, this.title)
             .setOrigin(0,0);
+        this.textTitle.setTint(descriptionTint);
 
-            this.descriptionTitle = this.context.add.text(this.x+40, this.y+16, this.description, {fontSize:'12px'})
-            .setOrigin(0,0);
-
+        this.descriptionTitle = this.context.add.text(this.x+40, this.y+16, this.description, {fontSize:'12px'})
+        .setOrigin(0,0);
+        this.descriptionTitle.setTint(descriptionTint);
 
     }
 
--- src/scripts/scenes/achievementsView.js
+++ src/scripts/scenes/achievementsView.js
@@ -22,22 +22,40 @@ export default class AchievementsView extends Phaser.Scene {
         { fontFamily:'Pixelart', fontSize: '30px', color: 'white', stroke: 'black', strokeThickness: 5 })
         .setInteractive().setOrigin(0,0)
         .on('pointerover', (e) => this.buttonHover(this.leftPageButton,'blue'))
-        .on('pointerout', (e) => this.buttonHover(this.leftPageButton,'white'));
+        .on('pointerout', (e) => this.buttonHover(this.leftPageButton,'white'))
+        .on(Phaser.Input.Events.POINTER_DOWN, () => {
+            this.page++;
+            if(this.page > this.maxPage) {
+                this.page = 0;
+            }
+            this.selectPage(this.page);
+        });
 
-        this.leftPageButton = this.add.text(290, 0, ">",
+        this.rightPageButton = this.add.text(290, 0, ">",
         { fontFamily:'Pixelart', fontSize: '30px', color: 'white', stroke: 'black', strokeThickness: 5 })
         .setInteractive().setOrigin(0,0)
-        .on('pointerover', (e) => this.buttonHover(this.leftPageButton,'blue'))
-        .on('pointerout', (e) => this.buttonHover(this.leftPageButton,'white'));
+        .on('pointerover', (e) => this.buttonHover(this.rightPageButton,'blue'))
+        .on('pointerout', (e) => this.buttonHover(this.rightPageButton,'white'))
+        .on(Phaser.Input.Events.POINTER_DOWN, () => {
+            this.page--;
+            if(this.page < 0) {
+                this.page = this.maxPage;
+            }
+            this.selectPage(this.page);
+        });;
 
-        this.pageText = this.add.text(160, 0, "Seite 1/?",
+        this.pageText = this.add.text(160, 0, `Seite 1/${this.maxPage+1}`,
         { fontFamily:'Pixelart', fontSize: '28px', color: 'white', stroke: 'black', strokeThickness: 5 }).setOrigin(0.5,0)
+
+
+        this.selectPage(0);
 
     }
 
     selectPage(page) {
         this.page = page;
-        this.pagedItems = Statistics.achievements.slice(page*this.maxItems,page*this.maxItems - 1)
+        this.pageText.text = `Seite ${this.page+1}/${this.maxPage+1}`;
+        this.pagedItems = Statistics.achievements.slice(page*this.maxItems,page*this.maxItems + this.maxItems)
         this.buildFrames();
     }
 
@@ -48,7 +66,7 @@ export default class AchievementsView extends Phaser.Scene {
         this.frames = [];
 
         for(let i = 0; i < this.pagedItems.length; i++) {
-            let frame = new AchievementFrame(this,30,50+i*38,this.pagedItems[i].type,this.pagedItems[i].amount,this.pagedItems[i].name,this.pagedItems[i].description,true);
+            let frame = new AchievementFrame(this,30,50+i*38,this.pagedItems[i].type,this.pagedItems[i].amount,this.pagedItems[i].name,this.pagedItems[i].description,this.pagedItems[i].done);
             this.frames.push(frame);
         }
     }
